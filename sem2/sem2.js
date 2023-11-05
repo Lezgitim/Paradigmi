e421a0fa2d25e9316bb1f7b4eed0eea4fa10e50d
Lezgitim <rinad_90@mail.ru>
2023-11-05 15:46:53 +0300

    2

---
+++ sem2/sem2.js
@@ -0,0 +1,12 @@
+function shKolnayaTetradZadniyVid(n) {
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= n; j++) {
+            let number = i * j;
+            console.log(`${i} * ${j} = ${number}`);
+        }
+    }
+}
+
+shKolnayaTetradZadniyVid(9);
+
+// Процедурная парадигма, потому что по другому не знаю как).
